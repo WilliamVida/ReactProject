ba576cb5701ab4ce098a626ddd5855f889c55016
WILLIAM VIDA - STUDENT <G00356773@gmit.ie>
2019-09-26 14:32:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import Content from './components/content';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar bg="primary" variant="dark">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Navbar>
+
+          <Switch>
+            <Route exact path="/" component={Content} />
+            <Route path="/read" component={Header} />
+            <Route path="/create" component={Footer} />
+          </Switch>
+
+          {/* <Header></Header>
+        <Content></Content>
+        <h1>Hello World!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        <Footer></Footer> */}
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/content.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <div className="App">
+            </div>
+        );
+    }
+}
+
+export default Content;
